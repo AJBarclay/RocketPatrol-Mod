130616856d2b8a2ebf73975d2656d37f0aaf9be2
Alexander Barclay <ajamesbarclay@gmail.com>
2021-04-16 17:15:52 -0700

    Added Rocket Class and setup

---
+++ code/Rocket Patrol Tutorial/src/prefabs/Rocket.js
@@ -0,0 +1,9 @@
+// Rocket prefab definition
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // adding object to existing scene
+        scene.add.existing(this);
+    }
+}
