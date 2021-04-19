0a3fe14afacb1cba99206b7bf36508515f1fca4a
Alexander Barclay <ajamesbarclay@gmail.com>
2021-04-18 22:39:04 -0700

    Collisions (and resets) added

--- code/Rocket Patrol Tutorial/src/prefabs/Rocket.js
+++ code/Rocket Patrol Tutorial/src/prefabs/Rocket.js
@@ -33,4 +33,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = game.config.height - borderUISize - borderPadding;
         }
     }
+
+    // resetting the rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
 }
